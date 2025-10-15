cef0808394f8d2f60f320f3d3bffb933d968c11c
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-15 11:27:23 +0600

    Solved 5 Tasks

--- exam.js
+++ exam.js
@@ -27,3 +27,31 @@ const isEvenOrOdd = number => {
 }
 isEvenOrOdd(9)
 
+// Task 3 -> Vowel Counter
+
+const countVowels = str => {
+    count = 0;
+    const newString = str.split('')
+    const letters = newString.map(element => {
+        if ('aeiouAEIOU'.includes(element)) {
+            count++
+        }
+    })
+
+}
+countVowels('Hello World')
+
+// Task 4 -> Sum of Array Elements 
+
+const sumArray = numbers => {
+    const sum = numbers.reduce((acc, currentValue) => {
+        acc += currentValue
+        return acc
+    })
+    console.log(sum)
+}
+sumArray([1, 2, 3, 4])
+
+// Task 5 -> Find the Largest Number 
+const findLargest = numbers => console.log(Math.max(...numbers))
+findLargest([5, 2, 8, 1, 9])
