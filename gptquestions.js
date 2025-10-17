bf1cc0b4465e17c52833915fac8675117c186171
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-17 22:32:34 +0600

    Solved All problems

--- gptquestions.js
+++ gptquestions.js
@@ -66,4 +66,26 @@ const palindromeCheck = str => {
    }
 }
 const texts = "A man, a plan, a canal: Panama";
-console.log(palindromeCheck(texts))
+console.log(palindromeCheck(texts)); 
+
+
+const fizzbuzz = n => {
+    let store = []
+    for(i=1; i<= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            store.push('FizzBuzz')
+        }
+        else if(i%3===0){
+            store.push('Fizz')
+        }
+        else if(i % 5 === 0){
+            store.push('Buzz')
+        }
+        else{
+            store.push(i)
+        }
+    }
+    return store;
+}
+
+console.log(fizzbuzz(15))
